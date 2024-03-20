920fec28265ae6a6fd26ee73102022b78151cf7f
fujitakeiei <masakazu777@ymail.ne.jp>
2024-03-20 11:27:32 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_19_024251) do
+ActiveRecord::Schema.define(version: 2024_03_20_110143) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
