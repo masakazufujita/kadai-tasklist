53430dea883f4a24dc52d9b0e7975fa5a9f2013f
fujitakeiei <masakazu777@ymail.ne.jp>
2024-03-26 03:05:56 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+# get 'toppages/index'
+# root to: 'tasks#index'
+  root to: 'toppages#index'
   
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
