6238fda72cbe840c7265a25cf1485529273ed494
fujitakeiei <masakazu777@ymail.ne.jp>
2024-03-26 08:29:53 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  root to: 'toppages#index'
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
 # get 'toppages/index'
 # root to: 'tasks#index'
-  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  # get 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
+
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
